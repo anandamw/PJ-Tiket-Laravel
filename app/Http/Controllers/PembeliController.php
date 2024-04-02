f2e0a1ee8285305e43d7f15a45a2b73f529aa9b3
nand4mw <ananda1850mw@gmail.com>
2024-04-02 15:59:50 +0700

    feat: update controller

--- app/Http/Controllers/PembeliController.php
+++ app/Http/Controllers/PembeliController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class KonsumenController extends Controller
+class PembeliController extends Controller
 {
     public function index()
     {
